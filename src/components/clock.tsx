9a3fc3ba11f8aeb2b8caa2aa8e9302da456410ce
latinrev <latinreve@gmail.com>
2023-03-27 00:20:20 -0500

    Switched to 24h format

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 export default function Clock() {
   const [timeZone, setTimeZone] = useState(Intl.DateTimeFormat().resolvedOptions().timeZone);
-  const [time, setTime] = useState("");
+  const [time, setTime] = useState(
+    `${new Date().getHours().toString().padStart(2, "0")}:${new Date().getMinutes().toString().padStart(2, "0")}`
+  );
 
   function refreshTime() {
-    setTime(new Date().toLocaleString("en-US", { timeZone, hour: "numeric", minute: "numeric", hour12: false }));
+    setTime(`${new Date().getHours().toString().padStart(2, "0")}:${new Date().getMinutes().toString().padStart(2, "0")}`);
   }
   useEffect(() => {
     let timeNow = setInterval(refreshTime, 1000);
